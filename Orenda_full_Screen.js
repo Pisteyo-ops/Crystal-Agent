0415de589ed27e3f4bbac218ef674b62fd08e5b4
Pisteyo-ops <vikassingh@pisteyo.com>
2025-11-12 02:05:50 +0700

    Update Orenda_full_Screen.js

--- Orenda_full_Screen.js
+++ Orenda_full_Screen.js
@@ -22,11 +22,7 @@ window.botpress.init({
     "allowFileUpload": true,
     "storageLocation": "localStorage",
     "soundEnabled": true,
-    "embeddedChatId": "bp-embedded-webchat",
-    "proactiveMessageEnabled": true,
-    "proactiveBubbleMessage": "Have a question? I can help!",
-    "proactiveBubbleTriggerType": "afterDelay",
-    "proactiveBubbleDelayTime": 10
+    "embeddedChatId": "bp-embedded-webchat"
   },
   "clientId": "d08d44d1-1b76-4275-9069-8a8cb366b734"
 });
